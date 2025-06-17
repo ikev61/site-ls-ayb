20fad446e8547dafd296220ff9c6fcc40cabb80e
ikev61 <kevineogbonna@gmail.com>
2025-06-17 13:29:11 +0100

    modify contect

--- pages/ctc.php
+++ pages/ctc.php
@@ -45,54 +45,6 @@
       <?php }?>
     </div>
   </div>
-
-  <div class="container">
-    <div class="row margin-lg-115t margin-md-75t margin-sm-45t">
-      <div class="col-md-12">
-        <div class="aheto-heading t-center aheto-heading--classic aheto-heading--b-c2">
-          <h2 class="aheto-heading__title          t-bold ">Get In Touch</h2>
-          <p class="aheto-heading__desc   ">Created from either wood or recycled materials, it can be moulded into just about </p>
-        </div>
-      </div>
-    </div>
-    <div class="row margin-lg-50t margin-lg-115b margin-md-40t margin-md-75b margin-sm-20t margin-sm-50b">
-      <div class="col-md-12">
-        <div class="aheto-form aheto-form--default aheto-form--business">
-          <form class="wpcf7-form">
-            <p>
-              <span class="wpcf7-form-control-wrap Name">
-                <input type="text" name="Name" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Gunsury">
-              </span>
-              <span class="wpcf7-form-control-wrap Email">
-                <input type="email" name="Email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-email" aria-invalid="false" placeholder="Email">
-              </span>
-              <span class="wpcf7-form-control-wrap Phone">
-                <input type="tel" name="Phone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-tel" aria-invalid="false" placeholder="Phone">
-              </span>
-              <span class="wpcf7-form-control-wrap Budget">
-                <select id="test13" name="Budget" class="wpcf7-form-control wpcf7-select" aria-invalid="false">
-                  <option value="Budget">Budget</option>
-                  <option value="$10">$10</option>
-                  <option value="$100">$100</option>
-                  <option value="$1 000">$1 000</option>
-                </select>
-              </span>
-            </p>
-            <p>
-            </p>
-            <p>
-              <span class="wpcf7-form-control-wrap Message">
-                <textarea name="Message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false" placeholder="Message"></textarea>
-              </span>
-            </p>
-            <p class="form-bth-holder">
-              <input type="submit" value="SEND MESSAGE" class="wpcf7-form-control wpcf7-submit ">
-            </p>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
   <?php require_once 'includes/foot.php'?>
 </body>
 
